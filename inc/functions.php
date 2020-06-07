db80045349a103a0f6796a42ad730a36294f4627
Bickeringmonkey <smeadows110280@gmail.com>
2020-06-07 09:10:56 +0100

    quotes added

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,103 @@
 // PHP - Random Quote Generator
-
+<?php
 // Create the Multidimensional array of quote elements and name it quotes
 // Each inner array element should be an associative array
-
-
+$quote[] = [
+    'quote' => '"Leave the gun. Take the cannoli."',
+    'name' => 'The Godfather',
+    'year' => '1972',
+];
+$quote[] = [
+    'quote' => '"May the Force be with you"',
+    'name' => 'Starwars',
+    'year' => '1977',
+];
+$quote[] = [
+    'quote' => '"My mama always said life was like a box of chocolates. You never know what you are gonna get."',
+    'name' => 'Forest Gump',
+    'year' => '1994',
+];
+$quote[] = [
+    'quote' => "'It's alive! It's alive!'",
+    'name' => 'Frankenstein',
+    'year' => '1931',
+];
+$quote[] = [
+    'quote' => "'You're gonna need a bigger boat.'",
+    'name' => 'Jaws',
+    'year' => '1975',
+];
+$quote[] = [
+    'quote' => '"Keep your friends close, but your enemies closer."',
+    'name' => 'The Godfather Part II',
+    'year' => '1974',
+];
+$quote[] = [
+    'quote' => "'Here's Johnny!'",
+    'name' => 'The Shining',
+    'year' => '1980',
+];
+$quote[] = [
+    'quote' => "'Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.'",
+    'name' => 'Albert Einstein',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => "It is strange that only extraordinary men make the discoveries, which later appear so easy and simple." ,
+    'name' => 'Georg C. Lichtenberg',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => "There is no law except the law that there is no law.",
+    'name' => 'John Archibald Wheeler',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"The saddest aspect of life right now is that gathers knowledge faster than society gathers wisdom."',
+    'name' => 'Isaac Asimov',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"A man who dares to waste one hour of time has not discovered the value of life."',
+    'name' => 'Charles Darwin',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"Success is walking from failure to failure with no loss of enthusiasm."',
+    'name' => 'Winston Churchill',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => "'Don't cry because it's over, smile because it happened.'",
+    'name' => 'Dr. Seuss',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change."',
+    'name' => 'Charles Darwin',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"Great minds discuss ideas; average minds discuss events; small minds discuss people."',
+    'name' => 'Eleanor Roosevelt',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"Live as if you were to die tomorrow. Learn as if you were to live forever."',
+    'name' => 'Mahatma Gandhi',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => '"The best revenge is massive success."',
+    'name' => 'Frank Sinatra',
+    'year' => '',
+];
+$quote[] = [
+    'quote' => "'The whole secret of a successful life is to find out what is one's destiny to do, and then do it.'",
+    'name' => 'Henry Ford',
+    'year' => '',
+];
 
 // Create the getRandomQuuote function and name it getRandomQuote
 
-
-
-// Create the printQuote funtion and name it printQuote
+// Create the printQuote funtion and name it printQuote?>
